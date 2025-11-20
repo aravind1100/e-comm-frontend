8488e50c1e9e6c4a26aa0250b026541685df975c
aravind1100 <aravindavd1999@gmail.com>
2025-11-20 17:47:19 +0530

    updated

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-
+import dotenv from dotenv;
+dotenv.config();
 
 const API = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}/api`,
